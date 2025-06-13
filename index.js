606d11ce4ed3c48c3e2547ec79b40bb899c02f80
AljurDev01 <aljurdev@gmail.com>
2025-06-13 16:48:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -462,7 +462,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'dashboard')));
+app.use(express.static(path.join(__dirname, 'dashboard', 'public')));
 
 app.post('/config', (req, res) => {
   const { appstate, commands, adminUid, prefix } = req.body;
