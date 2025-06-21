cea14b7cc5a7db726461276e3abe823b9664e9a8
AljurDev01 <aljurdev@gmail.com>
2025-06-21 16:45:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const { MongoClient } = require("mongodb");
 const fs = require("fs-extra");
 const path = require("path");
 const login = require("fbvibex");
+/*
 const apiHandler = require("./utils/apiHandler");
+*/
 const { handleAuroraCommand, loadAuroraCommands } = require("./core/aurora");
 loadAuroraCommands();
 const commands = new Map();
@@ -243,17 +245,17 @@ const handleMessage = async (api, event) => {
     setCooldown(senderID, commandName, cooldown || 3);
     try {
       if (command.execute) {
-        await command.execute(api, event, args, commands, prefix, config.admins, appState, sendMessage, apiHandler, usersData, globalData);
+        await command.execute(api, event, args, commands, prefix, config.admins, appState, sendMessage, usersData, globalData);
       } else if (command.run) {
-        await command.run({ api, event, args, apiHandler, usersData, globalData, admins: config.admins, prefix: prefix, db, commands });
-      } else if (command.config && command.config.onStart) {
+        await command.run({ api, event, args,  usersData, globalData, admins: config.admins, prefix: prefix, db, commands });
+    /*  } else if (command.config && command.config.onStart) {
         await command.config.onStart({
           api,
           event,
           args,
           message: {
             reply: (text, callback) => api.sendMessage(text, event.threadID, callback, event.messageID)
-          }
+          } */
         });
       }
       if (db && usersData.has(senderID)) {
